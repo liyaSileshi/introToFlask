35f44d99d6e839291334e678c607738267028046
liyaSileshi <liyasileshi@gmail.com>
2019-09-11 14:22:11 -0700

    gives random compliments

--- app.py
+++ app.py
@@ -1,35 +1,7 @@
-import requests
-from flask import Flask
-#
-app = Flask(__name__)
+from random import choice
 
-@app.route('/')
-def make_joke():
-    params = {
-    "q": "spongebob",
-    "Key": "CEED88MTT59M"
-    }
-    response = requests.get(
-    'https://api.tenor.com/v1/search',
-    params = params)
-    joke_json = r.json()
-    return "Hello worlkd"
+compliments = ['cute','funny','charming']
 
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-
-
-
-#spongebob gif query
-params = {
-"q": "trump",
-"Key": "CEED88MTT59M"
-}
-response = requests.get(
-'https://api.tenor.com/v1/search',
-params = params)
-
-gif_json= response.json()
+def get_compliment():
+    compliment = choice(compliments)
+    return f'Hello there user! You are so {compliment}!'
